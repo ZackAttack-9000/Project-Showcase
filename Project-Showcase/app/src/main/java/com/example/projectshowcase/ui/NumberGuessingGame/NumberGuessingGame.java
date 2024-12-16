1b68c1aad338bb8d52d5a2818a153a97524420a6
ZackAttack <zbolin01@gmail.com>
2024-12-15 18:26:11 -0600

    got the number guessing game finished, just need to finish the name gen and the madlib

--- Project-Showcase/app/src/main/java/com/example/projectshowcase/ui/NumberGuessingGame/NumberGuessingGame.java
+++ Project-Showcase/app/src/main/java/com/example/projectshowcase/ui/NumberGuessingGame/NumberGuessingGame.java
@@ -1,30 +1,73 @@
 package com.example.projectshowcase.ui.NumberGuessingGame;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.projectshowcase.R;
 
-public class NumberGuessingGame extends Fragment {
+import java.util.concurrent.atomic.AtomicInteger;
 
-    private NumberGuessingGameViewModel mViewModel;
 
-    public static NumberGuessingGame newInstance() {
-        return new NumberGuessingGame();
+    public class NumberGuessingGame extends Fragment {
+
+        private NumberGuessingGameViewModel mViewModel;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_number_guessing_game, container, false);
+            return view; // Return the inflated view
+        }
+
+        public NumberGuessingGame newInstance() {
+            return new NumberGuessingGame();
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            GuessingGame();
+        }
+
+
+        @SuppressLint("SetTextI18n")
+        void GuessingGame() {
+            AtomicInteger randomNumber = new AtomicInteger((int) (Math.random() * 100 + 1));
+            Button Guess = requireView().findViewById(R.id.button6);
+            Button NewGame = requireView().findViewById(R.id.button7);
+            TextView Reply = requireView().findViewById(R.id.textView20);
+            TextView Guesses = requireView().findViewById(R.id.textView21);
+            EditText GuessNumber = requireView().findViewById(R.id.editTextNumber);
+            AtomicInteger guesses = new AtomicInteger();
+
+            Guess.setOnClickListener(v -> {
+                guesses.getAndIncrement();
+                GuessNumber.getText().toString();
+                if (randomNumber.get() == Integer.parseInt(GuessNumber.getText().toString())) {
+                    Reply.setText("Correct!");
+                    Guesses.setText("You guessed the number in " + guesses + " guesses!");
+                } else if (randomNumber.get() > Integer.parseInt(GuessNumber.getText().toString())) {
+                    Reply.setText("Too low!");
+                } else {
+                    Reply.setText("Too high!");
+                }
+            });
+            NewGame.setOnClickListener(v -> {
+                randomNumber.set((int) (Math.random() * 100 + 1));
+                guesses.set(0);
+                Reply.setText("");
+                Guesses.setText("");
+            });
+        }
     }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_number_guessing_game, container, false);
-    }
-}
